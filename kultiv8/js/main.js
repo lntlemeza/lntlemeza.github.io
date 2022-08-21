f70f734b5a9cb2dc6317a9e0294a85fc542a995c
lntlemeza <l.ntlemeza@yahoo.com>
2022-08-21 04:19:03 +0200

    fixed lost conflicts

--- kultiv8/js/main.js
+++ kultiv8/js/main.js
@@ -8,4 +8,13 @@ card_flip.forEach(element => {
   });
 });
 
+$(".page-header ul a").on("click", function (e) {
+  // 1
+  e.preventDefault();
+  // 2
+  const href = $(this).attr("href");
+  // 3
+  $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+});
+
 
